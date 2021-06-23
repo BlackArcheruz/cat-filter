ce7acff30548e06464690f9936701f2856afe9df
Muhammadsiddiq-pyxel <breakingreport4@gmail.com>
2021-06-23 11:18:02 +0500

    update api

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     }
   }
   componentDidMount(){
-    fetch('http://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
     .then(users=> this.setState({cats: users}));
   }
